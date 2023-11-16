da98b638ccabc7ca2854a2576ecd192cae169e46
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2023-11-16 16:11:20 +0700

    Add homescreen, Transaction, Category UI

--- app/src/main/java/com/example/budgetbuddyapp/MainActivity.java
+++ app/src/main/java/com/example/budgetbuddyapp/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button a;
+        setContentView(R.layout.home_screen);
     }
 }
